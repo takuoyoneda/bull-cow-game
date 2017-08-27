4b5f17570682de61e13b89f81892b20b14ac06de
Takuo Yoneda <t.yoneda@shakeweb.co.uk>
2017-08-27 14:21:05 +0900

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Takuo Yoneda on 2017/08/27.
+//  Copyright © 2017 Takuo Yoneda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
